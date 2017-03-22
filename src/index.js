b30015df82ea638a2f4e7c58c631a202b36c87bb
Ethan Sharabi <ethans@wix.com>
2017-03-22 16:26:06 +0200

    expost Reducer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import {ActionTest} from './ActionTest';
+import Reducer from './Reducer';
 import {ReducerTest} from './ReducerTest';
 import * as WaitForAsyncsMiddleware from './WaitForAsyncsMiddleware';
 
-module.exports = {ActionTest, ReducerTest, WaitForAsyncsMiddleware};
+module.exports = {ActionTest, Reducer, ReducerTest, WaitForAsyncsMiddleware};
